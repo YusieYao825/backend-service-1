5428f24eb1002480580e72bb9139cd70ae793ed5
Yusie <xiyueyao@uw.edu>
2021-04-06 13:44:23 -0700

    update

--- src/main/resources/db/migration/V1_3__InsertTestData.sql
+++ src/main/resources/db/migration/V1_3__InsertTestData.sql
@@ -11,7 +11,7 @@ INSERT INTO static_subunit_info (`unit_name`, `subunit_name`) VALUES
 
 INSERT INTO static_approvers_info (`approver_netId`, `subunit_id`) VALUES
 ('sree', 1),
-('baosen', 1),
+('xiaotong', 1),
 ('baosen', 2),
 ('xiyueyao', 1),
 ('xiyueyao', 3),
